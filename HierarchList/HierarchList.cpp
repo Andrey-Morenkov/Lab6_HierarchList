8308d0e22c639a63643822ebe0097e6b14dfebc7
Andrey Morenkov <jedi-andr@mail.ru>
2016-03-01 23:35:33 +0300

    TText and base metods are realized

---
+++ HierarchList/HierarchList.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <SDKDDKVer.h>
+#include <stdio.h>
+#include <tchar.h>
+#include "TLink.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	return 0;
+}
+
---
+++ HierarchList/TLink.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "TLink.h"
+
+
+TLink::TLink()
+{
+}
+
+
+TLink::~TLink()
+{
+}
